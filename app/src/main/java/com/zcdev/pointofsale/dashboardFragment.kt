fb4745f4ffa958994107f168f54ec3b58e73bfe3
zakaria chergui <z.chergui@esi-sba.dz>
2021-12-01 08:13:19 +0100

    neww commit

---
+++ app/src/main/java/com/zcdev/pointofsale/dashboardFragment.kt
@@ -0,0 +1,26 @@
+package com.zcdev.pointofsale
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zcdev.pointofsale.data.models.Product
+
+
+class dashboardFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        var v= inflater.inflate(R.layout.fragment_dashboard, container, false)
+
+
+
+        return v
+    }
+
+
+}
--- app/src/main/java/com/zcdev/pointofsale/data/models/Product.kt
+++ app/src/main/java/com/zcdev/pointofsale/data/models/Product.kt
@@ -1,4 +1,21 @@
 package com.zcdev.pointofsale.data.models
 
 class Product {
+
+    var productName:String?=null
+    var productCode:Int?=null
+    var productQnt:Int?=null
+    var productPrice:Float?=null
+    var productImg:Int?=null
+
+
+    constructor(name:String,code:Int,qnt:Int,price:Float,img:Int){
+        this.productName=name
+        this.productCode=code
+        this.productQnt=qnt
+        this.productPrice=price
+        this.productImg=img
+    }
+
+
 }
