5c6c98bdaa9db5e909afb55993de0d2ea0ad45b7
Benmessabih Toufik <52804863+toufik7@users.noreply.github.com>
2021-12-03 11:56:47 +0100

    firebase connection added

--- app/src/main/java/com/zcdev/pointofsale/activitys/MainActivity.kt
+++ app/src/main/java/com/zcdev/pointofsale/activitys/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.zcdev.pointofsale.activitys
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.firebase.database.FirebaseDatabase
 import com.zcdev.pointofsale.R
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -16,7 +18,30 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.navController
         setupActionBarWithNavController(navController)
+
+
+
+
+
+        // Write a message to the database
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("Products")
+        var id= "1";
+        myRef.child(id).setValue("lait soummam")
+        id= "2";
+        myRef.child(id).setValue("Lait candiat")
+        id= "3";
+        myRef.child(id).setValue("Yago")
+        id= "4";
+        myRef.child(id).setValue("fromage")
+        id= "5";
+        myRef.child(id).setValue("fromage rouge")
     }
+
+
+
+
+
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
