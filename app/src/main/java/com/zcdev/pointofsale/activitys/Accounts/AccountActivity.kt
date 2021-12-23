7f6709a9da7719d4ad7ea987c8b6296f8f8751fd
zakaria chergui <z.chergui@esi-sba.dz>
2021-12-23 14:47:26 +0100

    simpleLogin

--- app/src/main/java/com/zcdev/pointofsale/activitys/Accounts/AccountActivity.kt
+++ app/src/main/java/com/zcdev/pointofsale/activitys/Accounts/AccountActivity.kt
@@ -4,20 +4,41 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.zcdev.pointofsale.R
 import com.zcdev.pointofsale.activitys.MainActivity
+import kotlinx.android.synthetic.main.activity_account.*
 
 class AccountActivity : AppCompatActivity() {
+    // Creating firebaseAuth object
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
 
         supportActionBar!!.hide()
+        // initialising Firebase auth object
+        auth = FirebaseAuth.getInstance()
+
 
     }
 
-
-    fun goTohome(view: View){
-        startActivity(Intent(this.applicationContext, MainActivity::class.java))
+    fun login(view:View) {
+        val email = textEmail.text.toString()
+        val pass = textPassword.text.toString()
+        // calling signInWithEmailAndPassword(email, pass)
+        // function using Firebase auth object
+        // On successful response Display a Toast
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                startActivity(Intent(this.applicationContext, MainActivity::class.java))
+                Toast.makeText(this, "Successfully LoggedIn", Toast.LENGTH_SHORT).show()
+            } else
+                Toast.makeText(this, "Log In failed ", Toast.LENGTH_SHORT).show()
+        }
     }
+
+
+
 }
